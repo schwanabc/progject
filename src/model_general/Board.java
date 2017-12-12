e22cac5a1947455470f51b6b3aed480a87488a5d
eqsk134 <eqsk134@gmail.com>
2017-12-12 21:27:32 +0700

    Swap argument in getTowerAttack

--- src/model_general/Board.java
+++ src/model_general/Board.java
@@ -107,7 +107,7 @@ public class Board implements IRenderable {
 				}
 			}
 	}
-	public static int getTowerAttack(int posY,int posX) {
+	public static int getTowerAttack(int posX,int posY) {
 		//Not finish
 		int countTower = 0;
 		for(int i=0;i<BOARD_ROW;i++) {
@@ -121,8 +121,8 @@ public class Board implements IRenderable {
 		}
 		
 		int towerATK = countTower;
-		if(board[posX][posY] == 1)
-			towerATK *= 5;
+		if(board[posY][posX] == 1)
+			towerATK *= 4;
 //		else if(board[posX][posY] == -1) {
 //			System.out.println("Find Hole");
 //			towerATK *= 0;
